e9f2c257f7d7207915b97a59f4e2629ebb028658
denakof <denakofahi_21@yahoo.com>
2021-08-24 23:21:51 +0300

    lab03

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const server = require("./src/server");
 const { db } = require("./src/models/index");
 db.sync()
   .then(() => {
-    server.start(4006);
+    server.start(process.env.PORT || 4006);
   })
   .catch(console.error);
